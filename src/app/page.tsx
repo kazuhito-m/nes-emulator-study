2cef3f5b4f7996f285aeef2a48bdaffbf3569726
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-09-23 21:43:04 +0900

    implement: Google Material UI を導入してみる。
    ここのやり方をそのまま実行。
    https://zenn.dev/ryosuke_horie/articles/ce00a0893c96d0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
 export default function Home() {
-  return <a href="/posts/first-post">this page!</a>;
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
 }
