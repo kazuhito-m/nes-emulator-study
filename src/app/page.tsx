b3db89e4a4691649ff61645138bf42de95e31875
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-10-11 22:22:09 +0900

    implement: 本来の用途の名前に置き換える。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,18 +69,7 @@ export default function Home() {
                   height: 500,
                 }}
               >
-                Recent Deposits
-                <Typography component="p" variant="h4">
-                  $3,024.00
-                </Typography>
-                <Typography color="text.secondary" sx={{ flex: 1 }}>
-                  on 15 March, 2019
-                </Typography>
-                <div>
-                  <Link color="primary" href="#">
-                    View balance
-                  </Link>
-                </div>
+                Controll Panel
               </Paper>
             </Grid>
 
