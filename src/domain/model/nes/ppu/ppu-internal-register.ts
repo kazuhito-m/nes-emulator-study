eac2fd024833e603369d749ea7c9d06c8db1a4f5
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-10-26 00:04:34 +0900

    implement: PpuInternalRegisterを移植/実装。

--- src/domain/model/nes/ppu/ppu-internal-register.ts
+++ src/domain/model/nes/ppu/ppu-internal-register.ts
@@ -16,93 +16,212 @@ export class PpuInternalRegister {
     private static readonly FINE_Y_MASK = 0b111000000000000;
 
     public setCoarseX(target: PpuInternalRegistertarget, data: number): void {
-        // TODO 実装。
+        // 下位 5 bit のみ有効
+        if ((0b11100000 & data) !== 0) throw new Error('under 5 bit invalid.');
+
+        const mask = PpuInternalRegister.COARSE_X_MASK;
+        if (target === PpuInternalRegistertarget.PpuInternalRegistertarget_t) {
+            this.t &= ~mask;
+            this.t |= data;
+        }
+        if (target === PpuInternalRegistertarget.PpuInternalRegistertarget_v) {
+            this.v &= ~mask;
+            this.v |= data;
+        }
     }
 
     public setCoarseY(target: PpuInternalRegistertarget, data: number): void {
-        // TODO 実装。
+        // 下位 5 bit のみ有効
+        if ((0b11100000 & data) !== 0) throw new Error('under 5 bit invalid.');
+
+        const writeData = data << 5;
+        const mask = PpuInternalRegister.COARSE_Y_MASK;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_t) {
+            this.t &= ~mask;
+            this.t |= writeData;
+        }
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_v) {
+            this.v &= ~mask;
+            this.v |= writeData;
+        }
     }
 
     public setNametableSelect(target: PpuInternalRegistertarget, data: number): void {
-        // TODO 実装。
+        // 下位 2 bit のみ有効
+        if ((0b11111100 & data) !== 0) throw new Error('under 2 bit invalid.');
+
+        const writeData = data << 10;
+        const mask = PpuInternalRegister.NAMETABLE_SELECT_MASK;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_t) {
+            this.t &= ~mask;
+            this.t |= writeData;
+        }
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_v) {
+            this.v &= ~mask;
+            this.v |= writeData;
+        }
     }
 
     public setFineY(target: PpuInternalRegistertarget, data: number): void {
-        // TODO 実装。
+        // 下位 3 bit のみ有効
+        if ((0b11111000 & data) !== 0) throw new Error('under 3 bit invalid.');
+
+        const writeData = data << 12;
+        const mask = PpuInternalRegister.FINE_Y_MASK;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_t) {
+            this.t &= ~mask;
+            this.t |= writeData;
+        }
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_v) {
+            this.v &= ~mask;
+            this.v |= writeData;
+        }
     }
 
     public setFineX(data: number): void {
-        // TODO 実装。
+        // 下位 3 bit のみ有効
+        if ((0b11111000 & data) !== 0) throw new Error('under 3 bit invalid.');
+
+        this.x = data;
     }
 
     public setW(data: boolean): void {
-        // TODO 実装。
+        this.w = data;
     }
 
     // PPUADDR 反映用(紛らわしいけど、 PPUADDR としては使わない、PPUADDR への書き込みと PPUSCROLL への書き込みを混ぜて使ってるゲームのため)
     public setUpperPpuAddr(data: number): void {
-        // TODO 実装。
+        // 上 2 bit をマスクする(https://wiki.nesdev.com/w/index.php/PPU_scrolling#Register_controls)
+        data &= 0b00111111;
+        const writeData = data << 8;
+
+        this.t &= 0xFF;
+        this.t |= writeData;
+
+        this.w = true;
     }
 
     public setLowerPpuAddr(data: number): void {
-        // TODO 実装。
+        // 下位8bit を更新するだけ
+        this.t &= 0xFF00;
+        this.t |= data;
+        this.v = this.t;
+
+        this.w = false;
     }
 
     public getCoarseX(target: PpuInternalRegistertarget): number {
         // TODO 実装。以下は仮。
-        return 0;
+        const mask = PpuInternalRegister.COARSE_X_MASK;
+        if (target === PpuInternalRegistertarget.PpuInternalRegistertarget_t)
+            return this.t & mask;
+        if (target === PpuInternalRegistertarget.PpuInternalRegistertarget_v)
+            return this.v & mask;
+
+        // unexpected default
+        // abort();
+        throw new Error('unexpected default.');
     }
 
     public getCoarseY(target: PpuInternalRegistertarget): number {
-        // TODO 実装。以下は仮。
-        return 0;
-    }
-    public getNametableSelect(target: PpuInternalRegistertarget): number {
-        // TODO 実装。以下は仮。
-        return 0;
+        const mask = PpuInternalRegister.COARSE_Y_MASK;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_t)
+            return (this.t & mask) >> 5;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_v)
+            return (this.v & mask) >> 5;
+
+        // unexpected default
+        // abort();
+        throw new Error('unexpected default.');
     }
 
+    public getNametableSelect(target: PpuInternalRegistertarget): number {
+        const mask = PpuInternalRegister.NAMETABLE_SELECT_MASK;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_t)
+            return (this.t & mask) >> 10;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_v)
+            return (this.v & mask) >> 10;
+
+        // unexpected default
+        // abort();
+        throw new Error('unexpected default.');
+    }
+
+
     public getFineY(target: PpuInternalRegistertarget): number {
-        // TODO 実装。以下は仮。
-        return 0;
+        const mask = PpuInternalRegister.FINE_Y_MASK;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_t)
+            return (this.t & mask) >> 12;
+        if (target == PpuInternalRegistertarget.PpuInternalRegistertarget_v)
+            return (this.v & mask) >> 12;
+
+        // unexpected default
+        // abort();
+        throw new Error('unexpected default.');
     }
 
     public getFineX(): number {
-        // TODO 実装。以下は仮。
-        return 0;
+        return this.x;
     }
 
     public getW(): boolean {
-        // TODO 実装。以下は仮。
-        return false;
+        return this.w;
     }
 
-    // 描画中のインクリメント
+    // 描画中のインクリメント(https://wiki.nesdev.com/w/index.php/PPU_scrolling#Wrapping_around)
     public incrementCoarseX(): void {
-        // TODO 実装。
+        const coarseX = this.getCoarseX(PpuInternalRegistertarget.PpuInternalRegistertarget_v);
+        // tile 31 の次は 0 にもどす、上で実装したアクセサーは使わずに nesdev wiki の疑似コードをそのまま使っちゃう
+        if (coarseX == 31) {
+            this.v &= ~0x001F;          // coarse X = 0
+            this.v ^= 0x0400;           // switch horizontal nametable
+        } else {
+            this.v++;
+        }
     }
 
     public incrementY(): void {
-        // TODO 実装。
+        // こちらも nesdev wiki の疑似コードをそのまま使っちゃう
+        if ((this.v & 0x7000) != 0x7000) {	    // if fine Y < 7
+            this.v += 0x1000;					// increment fine Y
+            return;
+        }
+
+        this.v &= ~0x7000;						// fine Y = 0
+        let y = (this.v & 0x03E0) >> 5;			// let y = coarse Y
+        if (y == 29) {
+            y = 0;                              // coarse Y = 0
+            this.v ^= 0x0800;                   // switch vertical nametable
+        } else if (y == 31) {
+            y = 0;                              // coarse Y = 0, nametable not switched
+        } else {
+            y += 1;							    // increment coarse Y
+        }
+        this.v = (this.v & ~0x03E0) | (y << 5);	// put coarse Y back into v
     }
 
-    // 現在のタイルと attribute table のアドレス取得
+    // 現在のタイルと attribute table のアドレス取得(https://wiki.nesdev.com/w/index.php/PPU_scrolling#Tile_and_attribute_fetching)
     public getTileAddress(): number {
-        // TODO 実装。以下は仮。
-        return 0;
+        return 0x2000 | (this.v & 0x0FFF);
     }
 
     public getAttributeAddress(): number {
-        // TODO 実装。以下は仮。
-        return 0;
+        const v = this.v;
+        return 0x23C0 | (v & 0x0C00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07);
     }
 
     // t の変更を v に反映
     public updateHorizontalV(): void {
-        // TODO 実装。
+        const HORIZONTAL_MASK = 0b000010000011111;
+        this.v &= ~HORIZONTAL_MASK;
+        const update = this.t & HORIZONTAL_MASK;
+        this.v |= update;
     }
 
     public updateVerticalV(): void {
-        // TODO 実装。
+        const VERTICAL_MASK = 0b111101111100000;
+        this.v &= ~VERTICAL_MASK;
+        const update = this.t & VERTICAL_MASK;
+        this.v |= update;
     }
 }
