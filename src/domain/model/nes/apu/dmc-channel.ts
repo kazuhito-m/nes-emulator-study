2701de0656726654f69318b680a232be1e297c99
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-10-26 15:21:52 +0900

    implement: DmcChannelを実装。

--- src/domain/model/nes/apu/dmc-channel.ts
+++ src/domain/model/nes/apu/dmc-channel.ts
@@ -1,4 +1,5 @@
 import { Apu } from "./apu";
+import { ApuTable } from "./apu-table";
 
 /**
  * DMC チャンネル。
@@ -40,28 +41,125 @@ export class DmcChannel {
     }
 
     public WriteRegister(value: number, addr: number): void {
-        // TODO 実装。
+        const offset = addr - this.m_BaseAddr;
+
+        switch (offset) {
+            case 0:
+                this.m_DmcIrqEnabled = ((value >> 7) & 1) === 1;
+                this.m_DmcLoop = ((value >> 6) & 1) === 1;
+                this.m_FreqTimer = new ApuTable().g_DmcFreqTable[value & 0b1111];
+                break;
+            case 1:
+                // デルタカウンタの初期値
+                this.m_Output = value & 0b01111111;
+                break;
+            case 2:
+                // DMCサンプリングを開始するとき、 アドレスカウンタにはレジスタ$4012 * $40 + $C000をセット(NES on FPGA)
+                this.m_AddrLoad = 0xC000 + value * 0x40;
+                break;
+            case 3:
+                // 残りバイトカウンタにはレジスタ$4013 * $10 + 1をセットします(NES on FPGA)
+                this.m_LengthLoad = ((value) * 0x10) + 1;
+                break;
+            default:
+                break;
+        }
     }
 
     // APU 全体レジスタ($4015, $4017 の書き込みで反映される値)
     public On4015Write(value: number): void {
-        // TODO 実装。
+        if ((value >> 4) & 1) {
+            this.m_Length = this.m_LengthLoad;
+            this.m_Addr = this.m_AddrLoad;
+        }
+        else {
+            this.m_Length = 0;
+        }
+
+        // 割り込みクリア
+        this.m_DmcIrqPending = false;
     }
 
     // DMC の Status bit は length の割り込みフラグの2つあるので出力引数で出す
-    public GetStatusBit(): number {
-        // TODO 実装。
+    public GetStatusBit(pOutValue: number): number {
+        let orValue = 0;
+        if (this.m_Length > 0) {
+            orValue |= (1 << 4);
+        }
+        if (this.m_DmcIrqPending) {
+            orValue |= (1 << 7);
+        }
+        return pOutValue | orValue;
     }
 
     // クロック(Apu クラスから呼び出すことを想定)
     // DMC はフレームシーケンサにクロックされることはない(？)のでタイマだけでOK
     // DMA で CPU が止まるかもしれないので止まった場合そのクロック数をかえす
     public ClockTimer(): number {
-        // TODO 実装。
+        let retCpuClock = 0;
+
+        if (this.m_FreqCounter > 0) {
+            this.m_FreqCounter--;
+        }
+        else {
+            this.m_FreqCounter = this.m_FreqTimer;
+
+            if (!this.m_OutputUnitSilent) {
+                // サイレンスフラグがクリアされていたら
+                if ((this.m_OutputShift & 1) && this.m_Output < 0x7e) {
+                    // デルタカウンタが 126 より小さいなら +2
+                    this.m_Output += 2;
+                }
+                if (!(this.m_OutputShift & 1) && this.m_Output > 1) {
+                    // デルタカウンタが 1 より大きいなら -2
+                    this.m_Output -= 2;
+                }
+            }
+
+            // シフトレジスタに入っている使用済みサンプルを捨てる
+            this.m_BitsInOutputUnit--;
+            this.m_OutputShift >>= 1;
+
+            if (this.m_BitsInOutputUnit == 0) {
+                this.m_BitsInOutputUnit = 8;
+                this.m_OutputShift = this.m_SampleBuffer;
+                this.m_OutputUnitSilent = this.m_IsSampleBufferEmpty;
+                this.m_IsSampleBufferEmpty = true;
+            }
+
+            // 必要なら DMA する
+            if (this.m_Length > 0 && this.m_IsSampleBufferEmpty) {
+                retCpuClock = 4;
+
+                this.m_SampleBuffer = this.m_pApu!.DmaReadFromCpu(this.m_Addr);
+                this.m_IsSampleBufferEmpty = false;
+
+                this.m_Addr++;
+                if (this.m_Addr > 0xFFFF) {
+                    // 0xFFFF を超えてたら 0x8000 に丸める
+                    this.m_Addr = 0x8000;
+                }
+
+                this.m_Length--;
+
+                if (this.m_Length == 0) {
+                    if (this.m_DmcLoop) {
+                        this.m_Length = this.m_LengthLoad;
+                        this.m_Addr = this.m_AddrLoad;
+                    }
+                    else if (this.m_DmcIrqEnabled) {
+                        this.m_DmcIrqPending = true;
+                        this.m_pApu!.GenerateCpuInterrupt();
+                    }
+                }
+            }
+        }
+
+        return retCpuClock;
     }
 
     // 出力
     public GetOutPut(): number {
-        // TODO 実装。
+        return this.m_Output;
     }
 }
