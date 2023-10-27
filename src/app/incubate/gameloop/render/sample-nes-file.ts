7f5212db5e4f50c6a5648b14b710519037c75f55
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-10-27 13:37:02 +0900

    implement: サンプルファイルを配列に変える実装。

--- src/app/incubate/gameloop/render/sample-nes-file.ts
+++ src/app/incubate/gameloop/render/sample-nes-file.ts
@@ -2,10 +2,6 @@ import helloWorldRomFile from './sample1.nes';
 
 export class SampleNesFile {
     public readBytes(): number[] {
-        // TODO 実装。以下はダミー。
-        console.log('importしてみた結果。');
-        console.log(helloWorldRomFile);
-        console.log(typeof helloWorldRomFile);
-        return [0];
+        return [...Buffer.from(helloWorldRomFile)];
     }
 }
