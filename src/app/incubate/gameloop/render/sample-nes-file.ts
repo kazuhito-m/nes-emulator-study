94740556022efaaef4d42dcb93a6cc10bc8c517a
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-10-27 20:31:29 +0900

    implement: 苦肉の策。テスト用のファイルはURIで出して、それをfetchするように。

--- src/app/incubate/gameloop/render/sample-nes-file.ts
+++ src/app/incubate/gameloop/render/sample-nes-file.ts
@@ -1,31 +1,7 @@
-// import helloWorldRomFile from './sample1.nes';
-import specialFile from './test.special';
-import testFile from './test.special'
+import helloWorldRomFileUri from './sample1.nes';
 export class SampleNesFile {
     public async readBytes(): Promise<number[]> {
-        console.log('specialFileが圧縮されて出力されたり？');
-        console.log(specialFile);
-
-        const arrayBuffer = await (await fetch(testFile)).arrayBuffer();
-        const simpleArray = [...new Uint8Array(arrayBuffer)];
-
-        console.log('もう、どうしようもないから…Fetchで。');
-        console.log('length:' + simpleArray.length)
-        for (let i = 0; i < 10; i++) {
-            console.log(i + ':' + simpleArray[i]);
-        }
-
-        // console.log('base64文字列？');
-        // console.log(helloWorldRomFile);
-
-        // const contexts = Buffer.from(helloWorldRomFile);
-
-        // console.log('byteLength:' + contexts.byteLength)
-        // for (let i = 0; i < 10; i++) {
-        //     console.log(i + ':' + contexts.at(i));
-        // }
-
-        // return [...Buffer.from(helloWorldRomFile)];
-        return [0];
+        const buffer = await (await fetch(helloWorldRomFileUri)).arrayBuffer();
+        return [...new Uint8Array(buffer)];
     }
 }
